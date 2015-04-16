693a1774c59362cb5c10ea4a4ee935fe5db6e044
Alim Ul Karim <devorg.bd@gmail.com>
2015-04-16 15:25:39 +0600

    error pages

--- WereViewApp/Controllers/ProfileController.cs
+++ WereViewApp/Controllers/ProfileController.cs
@@ -3,18 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WereViewApp.Models.EntityModel;
 using WereViewApp.Modules.DevUser;
 
-namespace WereViewApp.Controllers
-{
-    public class ProfileController : Controller
-    {
+namespace WereViewApp.Controllers {
+    public class ProfileController : Controller {
         // GET: Profile/username
-        public ActionResult Index(string username) {
+        public ActionResult Index(string username, int? page) {
             var user = UserManager.GetUser(username);
+            page = page ?? (page = 1);
             if (user != null) {
-                return View(user);
+                using (var db = new WereViewAppEntities()) {
+                    var apps = db.Apps
+                        .Where(n => n.PostedByUserID == user.UserID)
+                        .OrderByDescending(n => n.AppID);
+                    long pages = 0;
+                    long itemsInPage = AppConfig.Setting.PageItems;
+
+                    apps = DevMVCComponent.Database.Pagination.GetPageData<App>(apps,pages,)               
+                    return View(user);
+                }
             }
+            ViewBag.Reason = "User not found.";
+            return View("_NotExist");
         }
     }
 }
