3db35368a363c9c8edc1f268f87b30f7ecd121c5
Alim Ul Karim <devorg.bd@gmail.com>
2015-04-17 21:11:39 +0600

    sitemaps added

---
+++ WereViewApp/Modules/Sitemaps/ISitemapGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace WereViewApp.Modules.Sitemaps
+{
+    public interface ISitemapGenerator
+    {
+        XDocument GenerateSiteMap(IEnumerable<ISitemapItem> items);
+    }
+}
