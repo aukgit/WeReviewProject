64ae6075e05c2d030759ed82f89803b5d631cec6
Alim Ul Karim <devorg.bd@gmail.com>
2015-04-17 22:27:49 +0600

    refactored names

--- WereViewApp/Controllers/AdvanceController.cs
+++ WereViewApp/Controllers/AdvanceController.cs
@@ -1,33 +1,27 @@
-﻿using WereViewApp.Models.Context;
-using WereViewApp.Modules.UserError;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using WereViewApp.Models.EntityModel;
+﻿using System.Web.Mvc;
 using WereViewApp.Filter;
+using WereViewApp.Models.EntityModel;
+using WereViewApp.Modules.UserError;
 
 namespace WereViewApp.Controllers {
-
     //[CompressFilter]
     [CacheFilter(Duration = 8)]
     public abstract class AdvanceController : Controller {
+        internal ErrorCollector ErrorCollector;
         internal readonly WereViewAppEntities db;
-        internal ErrorCollector _errorCollector;
-
 
         public AdvanceController() {
-
         }
+
         public AdvanceController(bool applicationDbContextRequried) {
             if (applicationDbContextRequried) {
                 db = new WereViewAppEntities();
             }
         }
+
         public AdvanceController(bool applicationDbContextRequried, bool errorCollectorRequried) {
             if (errorCollectorRequried) {
-                _errorCollector = new ErrorCollector();
+                ErrorCollector = new ErrorCollector();
             }
             if (applicationDbContextRequried) {
                 db = new WereViewAppEntities();
@@ -38,8 +32,8 @@ namespace WereViewApp.Controllers {
             if (db != null) {
                 db.Dispose();
             }
-            if (_errorCollector != null) {
-                _errorCollector.Dispose();
+            if (ErrorCollector != null) {
+                ErrorCollector.Dispose();
             }
             base.Dispose(disposing);
         }
--- WereViewApp/Controllers/BasicController.cs
+++ WereViewApp/Controllers/BasicController.cs
@@ -1,29 +1,23 @@
-﻿using WereViewApp.Models.Context;
+﻿using System.Web.Mvc;
+using WereViewApp.Models.Context;
 using WereViewApp.Modules.UserError;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using WereViewApp.Filter;
 
 namespace WereViewApp.Controllers {
-
     //[CompressFilter(Order = 1)]
 
     public abstract class BasicController : Controller {
-        internal readonly ApplicationDbContext db;
         internal ErrorCollector _errorCollector;
-
+        internal readonly ApplicationDbContext db;
 
         public BasicController() {
-
         }
+
         public BasicController(bool applicationDbContextRequried) {
             if (applicationDbContextRequried) {
                 db = new ApplicationDbContext();
             }
         }
+
         public BasicController(bool applicationDbContextRequried, bool errorCollectorRequried) {
             if (errorCollectorRequried) {
                 _errorCollector = new ErrorCollector();
