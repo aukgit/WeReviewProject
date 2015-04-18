0148a0165a2f2047df2825a784902fef48177377
Alim Ul Karim <devorg.bd@gmail.com>
2015-04-18 22:53:30 +0600

    errors controller cleaned up

--- WereViewApp/Controllers/ErrorsController.cs
+++ WereViewApp/Controllers/ErrorsController.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿#region using block
+
 using System.Web.Mvc;
+
+#endregion
 
 namespace WereViewApp.Controllers {
     [OutputCache(CacheProfile = "YearNoParam")]
@@ -11,33 +11,43 @@ namespace WereViewApp.Controllers {
         public ActionResult Index() {
             return View("_Error");
         }
+
         public ActionResult Error_400() {
             return View("_400");
         }
+
         public ActionResult Error_403() {
             return View("_403");
         }
+
         public ActionResult Error_404() {
             return View("_404");
         }
+
         public ActionResult Error_414() {
             return View("_414");
         }
+
         public ActionResult Error_415() {
             return View("_415");
         }
+
         public ActionResult Error_429() {
             return View("_429");
         }
+
         public ActionResult Error_431() {
             return View("_429");
         }
+
         public ActionResult Error_451() {
             return View("_451");
         }
+
         public ActionResult Error_500() {
             return View("_500");
         }
+
         public ActionResult Error_502() {
             return View("_502");
         }
