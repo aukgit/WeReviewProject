826e0f7b1db933b916d050092e0ad408d28aebd7
Alim Ul Karim <devorg.bd@gmail.com>
2015-04-19 15:20:42 +0600

    error max try increased to 60

--- WereViewApp/Controllers/SearchController.cs
+++ WereViewApp/Controllers/SearchController.cs
@@ -21,15 +21,15 @@ namespace WereViewApp.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        //[OutputCache(CacheProfile = "Long", VaryByParam = "SearchQuery")]
+        [OutputCache(CacheProfile = "Long", VaryByParam = "SearchQuery")]
         public ActionResult Index(string SearchQuery) {
-            var max = 3;
+            var max = 60;
 
             SearchViewModel search = new SearchViewModel();
             var algorithms = new Algorithms();
             //ViewBag.isPostBack = true;
             if (!string.IsNullOrWhiteSpace(SearchQuery)) {
-                if (AppVar.Setting.IsInTestingEnvironment) {
+                if (!AppVar.Setting.IsInTestingEnvironment) {
                     if (SessionNames.IsValidationExceed("SearchingFormCount", max)) {
                         ViewBag.Reason = "You have exceed your search cases. Perhaps you should try tomorrow.";
                         return View("_429");
