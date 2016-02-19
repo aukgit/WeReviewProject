267ae02efa01c0ee678fd34d2d25f0d1f4b709cd
Alim Ul Karim <devorg.bd@gmail.com>
2016-02-19 19:14:41 +0600

    changes to implement moderation
    changes to implement moderation

--- WereViewApp/Modules/Extensions/IdentityExtension/ExtentsionUserIdentityMethods.cs
+++ WereViewApp/Modules/Extensions/IdentityExtension/ExtentsionUserIdentityMethods.cs
@@ -1,10 +1,26 @@
 ﻿using System.Security.Principal;
 using Microsoft.AspNet.Identity;
+using WereViewApp.Models.POCO.Identity;
+using WereViewApp.Modules.DevUser;
 
 namespace WereViewApp.Modules.Extensions.IdentityExtension {
     public static class ExtentsionUserIdentityMethods {
         public static long GetUserID(this IIdentity identity) {
             return long.Parse(identity.GetUserId());
+        }
+
+        public static ApplicationUser GetUser(this IPrincipal user) {
+            if (user.Identity.IsAuthenticated) {
+                return UserManager.GetCurrentUser();
+            }
+            return null;
+        }
+        public static long GetUserID(this IPrincipal user) {
+            if (user.Identity.IsAuthenticated) {
+                var userObject = GetUser(user);
+                return userObject.UserID;
+            }
+            return -1;
         }
     }
 }
