573a341f101aabbcea20c26dafd6f6fb2388a344
Alim Ul Karim <devorg.bd@gmail.com>
2016-02-19 09:16:26 +0600

    AppModerate Template ready
    AppModerate Template ready
    - Blocking mechanism should be put into the algorithm.
    - Feature logic should also be placed in the algorithm.

--- WereViewApp/Areas/Admin/Controllers/AppModerateController.cs
+++ WereViewApp/Areas/Admin/Controllers/AppModerateController.cs
@@ -10,32 +10,46 @@ using WereViewApp.Models.ViewModels;
 namespace WereViewApp.Areas.Admin.Controllers {
     public class AppModerateController : AdvanceController {
 
-        public AppModerateController():base(true) {
-            
+        public AppModerateController()
+            : base(true) {
+
         }
+
+        private const string TempAppKey = "app-moderate";
+        private const string TempAppFeaturedKey = "app-moderate-is-featured";
+
         // GET: Admin/AppModerate
         public ActionResult Index(long id) {
             var appModerateModel = new AppModerateViewModel() {
                 AppId = id
             };
 
-            var app  = db.Apps.Find(id);
+            var app = db.Apps.Find(id);
             if (app == null) {
                 return HttpNotFound();
             }
-            ViewBag.app = app;
+            TempData[TempAppKey] = app;
             appModerateModel.App = app;
             appModerateModel.IsBlocked = app.IsBlocked;
-            appModerateModel.IsFeatured = db.FeaturedImages.Any(n=> n.IsFeatured && n.AppID == id);
+            appModerateModel.IsFeatured = db.FeaturedImages.Any(n => n.IsFeatured && n.AppID == id);
+            TempData[TempAppFeaturedKey] = appModerateModel.IsFeatured;
             return View(appModerateModel);
         }
         [HttpPost]
         public ActionResult Index(AppModerateViewModel model) {
-            var app = ViewBag.app as App ;
+              var app =         TempData[TempAppKey]  as App;
+            var isFeaturedPreviously = (bool)TempData[TempAppFeaturedKey];
+
             if (app != null) {
-                if(app.is)
+                if (app.IsBlocked != model.IsBlocked) {
+                    // needs to update
+                }
+                if (isFeaturedPreviously != model.IsFeatured) {
+                    // needs to update
+                }
             }
-        
+            AppVar.SetSavedStatus(this.ViewBag, "You have successfully moderated this app.");
+            return View(model);
         }
     }
 }
