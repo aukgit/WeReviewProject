ed0eb95c3ef5d2435211818e7d522c27609d8c49
Alim Ul Karim <devorg.bd@gmail.com>
2016-03-20 10:38:15 +0600

    AppSummary Algorithm added + Config tab names renamed according to the document.
    AppSummary Algorithm added + Config tab names renamed according to the
    document.

--- WereViewApp/Areas/Admin/Controllers/HomeController.cs
+++ WereViewApp/Areas/Admin/Controllers/HomeController.cs
@@ -1,11 +1,19 @@
 ﻿using System.Web.Mvc;
 using System.Web.UI;
+using WereViewApp.Models.ViewModels;
+using WereViewApp.WereViewAppCommon;
 
 namespace WereViewApp.Areas.Admin.Controllers {
     [OutputCache(NoStore = true, Location = OutputCacheLocation.None)]
     public class HomeController : Controller {
         public ActionResult Index() {
-            return View();
+            var appSummaryModel = Session["admin-app-summary"] as AppSummaryViewModel;
+            if (appSummaryModel == null) {
+                var algorithms = new Algorithms();
+                appSummaryModel = algorithms.GetAppsSummary();
+                Session["admin-app-summary"] = appSummaryModel;
+            }
+            return View(appSummaryModel);
         }
     }
 }
