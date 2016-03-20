cd798630de1834e68931f72cfa63f5b3fa5ad3fd
Alim Ul Karim <devorg.bd@gmail.com>
2016-03-21 01:46:51 +0600

    Javascript Mvc framework implemented project is working
    Javascript Mvc framework implemented project is working

---
+++ WereViewApp/JavaScript-Mvc-framework/app.global.js
@@ -0,0 +1,8 @@
+﻿; $.app = $.app || {};
+$.app.global = {
+    /**
+     * global methods collection 
+     * execute methods as per necessary, these methods will not run automatically.
+     */
+
+};
---
+++ WereViewApp/JavaScript-Mvc-framework/schema/url.js
@@ -0,0 +1,8 @@
+﻿; $.app = $.app || {};
+; $.app.schema = $.app.schema || {};
+;$.app.schema.url = {
+    add: null,
+    edit: null,
+    delete: null,
+    save:null
+};
