c0c9776de72cbe19c81a6a38d75a564c5ed43c1b
Alim Ul Karim <devorg.bd@gmail.com>
2016-03-23 17:59:23 +0600

    Cleaned up algorithms

--- WereViewApp/Algorithms/AdminAlgorithm.cs
+++ WereViewApp/Algorithms/AdminAlgorithm.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace WereViewApp.WereViewAppCommon {
+﻿namespace WereViewApp.WereViewAppCommon {
     public class AdminAlgorithm {
         #region Navigation Controller
+
         #endregion
     }
 }
