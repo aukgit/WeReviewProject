4a788bd5e6378f69d89c68f45f3c6df80481c8fb
Khaled Md Tuhidul Hossain <mth.tuhin@gmail.com>
2018-03-16 18:57:16 +0600

    css file location added in gulp.config
    asd

--- WereViewApp/gulp.config.js
+++ WereViewApp/gulp.config.js
@@ -12,7 +12,10 @@
         minJs: "js-min.js", /* Minified JS file name */
         minJsDestination: "./lib/js/", /* Minified JS file destination */
         css: [
-            "./lib/css/*.css" /* List of css files that need to be minified */
+            "./Content/styles/*.css",
+            "./Content/styles/css/*css",
+            "./Content/styles/css/Upload/*.css",
+            "./Content/styles/css/Upload/Tagit/*.css"/* List of css files that need to be minified */
         ],
         minCss: "css-min.css", /* Minified CSS file name */
         minCssDestination: "./lib/css/" /* Minified CSS file destination */
