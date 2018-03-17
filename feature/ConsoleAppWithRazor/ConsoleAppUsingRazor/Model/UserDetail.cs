6651838b956c2c962853982213609450ce402d8f
Khaled Md Tuhidul Hossain <mth.tuhin@gmail.com>
2018-03-17 16:30:04 +0600

    Razor With C# Console Application

---
+++ feature/ConsoleAppWithRazor/ConsoleAppUsingRazor/Model/UserDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateEmailUsingRazor.Model
+{
+
+    public class UserDetail
+    {
+
+        public UserDetail()
+        {
+            PurchasedItems = new List<string>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public List<string> PurchasedItems { get; set; }
+
+    }
+
+}
---
+++ feature/ConsoleAppWithRazor/ConsoleAppUsingRazor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenerateEmailUsingRazor.Model;
+using RazorEngine.Templating;
+
+namespace GenerateEmailUsingRazor
+{
+    class Program
+    {
+        static readonly string TemplateFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "EmailTemplates");
+
+        static void Main(string[] args)
+        {
+            var model = GetUserDetail();
+
+
+            var emailTemplatePath = Path.Combine(TemplateFolderPath, "InviteEmailTemplate.cshtml");
+
+            var templateService = new TemplateService();
+            var emailHtmlBody = templateService.Parse(File.ReadAllText(emailTemplatePath), model, null, null);
+
+            
+            Console.WriteLine(emailHtmlBody);
+
+            Console.ReadLine();
+
+        }
+
+        private static UserDetail GetUserDetail()
+        {
+            var model = new UserDetail()
+                            {
+                                Id = 1,
+                                Name = "Khaled ",
+                                Address = "Dhaka"
+                            };
+
+
+            for (int i = 1; i <= 10; i++)
+            {
+                model.PurchasedItems.Add("Item No " + i);
+            }
+            return model;
+        }
+    }
+}
